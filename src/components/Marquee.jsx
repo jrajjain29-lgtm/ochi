474d212b15031d9e1d3801791f37c263cfdcbb51
Sujal jain <jrajjain29@gmail.com>
2026-05-31 16:30:49 +0530

    changes succesfiully

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Marquee = () => {
 
   return (
-    <div data-scroll data-scroll-section data-scroll-speed=".2" className='w-full pt-25 pb-20 rounded-t-3xl bg-[#004D43]'>
+    <div data-scroll data-scroll-section data-scroll-speed="0.1" className='w-full pt-25 pb-20 rounded-t-3xl bg-[#004D43]'>
         <div className='text flex border-t-2 border-b-2 border-zinc-400 whitespace-nowrap overflow-hidden'>
             <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: "linear",duration: 10}} className='text-[20vw] leading-none text-white font-["Founders_Grotesk"] font-semibold pt-10 -mb-[5vw] uppercase tracking-tighter pr-20'>WE ARE OCHI</motion.h1>
             <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: "linear",duration: 10}} className='text-[20vw] leading-none text-white font-["Founders_Grotesk"] font-semibold pt-10 -mb-[5vw] uppercase tracking-tighter pr-20'>WE ARE OCHI</motion.h1>
